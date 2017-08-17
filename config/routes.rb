7dbec9247095aca8c9838bc6dbb6e36ad84f2187
Sabrina Wilbert <sabrinawilbert@gmail.com>
2017-08-17 12:49:33 -0700

    added delete and toggle complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
-  get 'homepages/index'
-  root to: 'homepages#index'
+  # get 'homepages/index'
+  get 'todos/index' =>'todos#index', as: 'index'
 
-  get 'todos/index'
+  # get 'todos/index'
+  # root to: 'todos#index'
 
   get 'todos/show/:id' =>'todos#show', as: 'show'
 
   get 'todos/new' =>'todos#new', as: 'new'
-
-  get 'todos/destroy'
 
   get 'todos/:id/edit' => 'todos#edit', as: 'edit'
 
   patch 'todos/:id/update' => 'todos#update', as: 'update'
 
   post 'todos/create' =>'todos#create', as: 'create'
+
+  patch 'tasks/:id/toggle_completed' => 'tasks#toggle_completed', as:'toggle'
+
+  delete 'todos/:id/destroy' => 'todos#destroy', as: 'delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
