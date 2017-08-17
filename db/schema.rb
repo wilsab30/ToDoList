8c9c7e9eb909ffc28092ea9bae3fca6f942b2eb9
Sabrina Wilbert <sabrinawilbert@gmail.com>
2017-08-17 14:02:50 -0700

    added due date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816224217) do
+ActiveRecord::Schema.define(version: 20170817195903) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170816224217) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean "finished"
+    t.date "due_date"
   end
 
 end
