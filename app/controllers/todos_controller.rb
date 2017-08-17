edd21fdafac85290d0c44e461daaf7b5c348ff4e
Sabrina Wilbert <sabrinawilbert@gmail.com>
2017-08-16 17:27:13 -0700

    Able to add new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,11 +3,40 @@ class TodosController < ApplicationController
   end
 
   def show
+    @mytodo = TodoItem.first
   end
 
   def new
+    @mytodo = TodoItem.new
+  end
+
+  def create
+    s1 = TodoItem.new
+    s1.title = params[:todo_item][:title]
+    s1.description = params[:todo_item][:description]
+    s1.finished = params[:todo_item][:finished]
+    s1.save
+    redirect_to action: "index"
+  end
+
+  def edit
+    @mytodo = TodoItem.find(params[:id].to_i)
+  end
+  def update
+    @mytodo = TodoItem.find(params[:id].to_i)
+    @mytodo[:title] = params[:todo_item]["title"]
+    @mytodo[:description] = params[:todo_item]["description"]
+    @mytodo[:finished] = params[:todo_item]["finished"]
+    @mytodo.save
+    if @mytodo.finished == true
+      @mytodo.completed_at = DateTime.now
+      @mytodo.save
+  end
+  redirect_to action: "index"
   end
 
   def destroy
   end
+
+
 end
